4a1f28e4e627e8ace6da4a65696c16f6d07a6a5a
stanley823 <stanley823@gmail.com>
2019-11-28 22:46:41 +0800

    update css - RWD

---
+++ build/precache-manifest.df86ffbdc09a682dcc4cd3a9814a8df4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b7aeb10fa03b251f8c3301638c0a42",
+    "url": "/github-repos-search/index.html"
+  },
+  {
+    "revision": "0a247018cd8ee3b9840d",
+    "url": "/github-repos-search/static/css/main.4700f572.chunk.css"
+  },
+  {
+    "revision": "ee0ba4fc0863e01c1d58",
+    "url": "/github-repos-search/static/js/2.afba43ce.chunk.js"
+  },
+  {
+    "revision": "0a247018cd8ee3b9840d",
+    "url": "/github-repos-search/static/js/main.abcad536.chunk.js"
+  },
+  {
+    "revision": "d6b23ffc00eebd6a35df",
+    "url": "/github-repos-search/static/js/runtime-main.1029b634.js"
+  }
+]);
